b5693b20ed6795df270d418da71d2ca614f5c766
MoraaS <salmamoraa@gmail.com>
2019-06-19 11:13:02 +0300

    add lipa na mpesa online file

--- application/access_token.php
+++ application/access_token.php
@@ -24,29 +24,4 @@
     curl_close($curl);
 
 
-
-
-//     $url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-
-//     $credentials = base64_encode('qvAGTb7BLEKe3GYZDywHyHGr7fA3vJ3W:aouAieLaYWiStnGr');
-//     $header = ['Authorization: Basic '.$credentials];
-
-//     $curl = curl_init();
-//     curl_setopt($curl, CURLOPT_URL, $url);
-//     curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-//     curl_setopt($curl, CURLOPT_HTTPHEADER,$header); //setting a custom header
-//     curl_setopt($curl, CURLOPT_HEADER,FALSE);
-//     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-//     $curl_response = curl_exec($curl);
-   
-//     $result =json_decode($curl_response);
-   
-//     // $access_token = $curl_response->access_token;
-//     echo ($result);
-//     die(); -->
-    
-
-
-// 
-   
+?>   
---
+++ application/lipa.php
@@ -0,0 +1,70 @@
+<?php
+//acesstoken
+$consumerKey ='qvAGTb7BLEKe3GYZDywHyHGr7fA3vJ3W';
+$consumerSecret ='aouAieLaYWiStnGr';
+
+$headers = ['Content-Type:application/json; charset=utf8'];
+
+$access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+
+$curl = curl_init($access_token_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+
+$result = curl_exec($curl);
+
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$result = json_decode($result);
+
+$access_token = $result->access_token;
+curl_close($curl);
+
+//initiating the transaction
+
+//Defining variables
+$initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+$BusinessShortCode = '174379';
+$Timestamp = date('YmdHis');
+$PartyA = '+254713623572'; // This is your phone number, 
+$AccountReference = 'is_Cart01';
+$TransactionDesc = 'Cart Payment for online';
+$Amount = '1';
+$CallBackURL = 'http://iventsoft.com/salma/lipakash/application/callback_url.php';
+# Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+$Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
+$Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'; 
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $initiate_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+
+$curl_post_data = array(
+  //Fill in the request parameters with valid values
+  'BusinessShortCode' => $BusinessShortCode,
+  'Password' => $Password,
+  'Timestamp' => $Timestamp,
+  'TransactionType' => 'CustomerPayBillOnline',
+  'Amount' => $Amount,
+  'PartyA' => $PartyA,
+  'PartyB' => $BusinessShortCode,
+  'PhoneNumber' => $PartyA,
+  'CallBackURL' => $CallBackURL,
+  'AccountReference' => $AccountReference,
+  'TransactionDesc' => $TransactionDesc
+);
+
+$data_string = json_encode($curl_post_data);
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+$curl_response = curl_exec($curl);
+print_r($curl_response);
+
+echo $curl_response;
+?>
